12ea611e774d5692c43fc39ed778e81af46ab69a
Daniel Pedroza <d.pedroza@uniandes.edu.co>
2024-11-24 19:03:06 -0500

    Instalando TypeORM y el driver de base de datos

--- parcial_2/src/app.module.ts
+++ parcial_2/src/app.module.ts
@@ -1,10 +1,25 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+/* Añadir los imports de las entidades*/
 
 @Module({
-  imports: [],
+  imports: [/*Modulos van aca con ,*/
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'localhost',
+      port: 5432,
+      username: 'postgres',
+      password: '7659',
+      database: 'Parcial2',
+      entities: [/*Entidades*/],
+      dropSchema: true,
+      synchronize: true,
+      keepConnectionAlive: true,
+    })
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule { }
